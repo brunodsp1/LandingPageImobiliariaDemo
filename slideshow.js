633dfb154997887b46455640b2113f9f96c2b003
brunodsp <drakearcanjo@gmail.com>
2021-01-09 00:31:28 -0300

    fixed images,  border-wrapper and added fadein on scroll

--- slideshow.js
+++ slideshow.js
@@ -1,23 +1,30 @@
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
+var slideIndex = [1, 1];
+var slideId = ["mySlides1", "mySlides2"];
 showSlides(1, 0);
 showSlides(1, 1);
 
 function currentSlide(n, no) {
-    showSlides(slideIndex[no] = n, no);
-  }
+  showSlides((slideIndex[no] = n), no);
+}
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+  showSlides((slideIndex[no] += n), no);
 }
 
 function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  var slides = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex[no] = 1;
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex[no] = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex[no] - 1].style.display = "block";
 }
+
+
